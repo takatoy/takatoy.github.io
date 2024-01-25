020534a296163e733bca66ae0ee55290148c9c60
Takato Yamazaki <takato.yamazaki@sbintuitions.co.jp>
2024-01-25 21:57:05 +0900

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -6,63 +6,93 @@
         src="/img/portrait.jpg"
       />
       <div class="flex flex-col">
-        <h1 class="font-bold text-2xl md:text-4xl">Takato Yamazaki</h1>
+        <h1 class="font-bold text-2xl text-white md:text-4xl">
+          Takato Yamazaki
+        </h1>
         <div class="text-neutral-400 mt-1 md:text-lg md:mt-2">
           <a
             class="hover:text-neutral-300 hover:underline"
             href="mailto:takato@yamazaki.dev"
+            target="_blank"
             >takato@yamazaki.dev</a
           >
         </div>
         <div class="mt-4 md:text-lg">
-          I am a researcher and engineer working at
+          I'm an engineer and researcher working at
           <a
             class="text-orange-300 hover:text-orange-200 hover:underline text-nowrap"
             href="https://www.lycorp.co.jp/"
+            target="_blank"
             >LY Corporation</a
           >
           and
           <a
             class="text-orange-300 hover:text-orange-200 hover:underline text-nowrap"
             href="https://www.sbintuitions.co.jp/"
+            target="_blank"
             >SB Intuitions Corp</a
           >. My research interest is in dialogue systems.
         </div>
       </div>
     </div>
     <div class="mt-14">
-      <h2 class="font-bold text-xl md:text-2xl">Work Experience</h2>
-      <ul class="flex flex-col gap-4 mt-4">
-        <li class="flex gap-6 rounded-lg p-4 bg-neutral-700">
-          <div class="">2023.11 - Present</div>
-          <div class="shrink flex flex-col">
-            <div class="font-bold text-xl">SB Intuitions Corporation</div>
-            <div class="text-neutral-400">Engineer</div>
-          </div>
-        </li>
-        <li class="flex gap-6 rounded-lg p-4 bg-neutral-700">
-          <div class="">2023.10 - Present</div>
-          <div class="shrink flex flex-col">
-            <div class="font-bold text-xl">
-              LY Corporation
-              <span class="text-neutral-400 text-sm"
-                >(formerly LINE Corporation)</span
-              >
-            </div>
-            <div class="text-neutral-400">Senior Engineer</div>
-          </div>
-        </li>
-        <li class="flex gap-6 rounded-lg p-4 bg-neutral-700">
-          <div class="">2021.04 - 2023.10</div>
-          <div class="shrink flex flex-col">
-            <div class="font-bold text-xl">LINE Corporation</div>
-            <div class="text-neutral-400">
-              Senior Engineer <span class="text-xs">(2021.04 - 2023.10)</span> /
-              Engineer
-              <span class="text-xs">(2023.09 - 2023.10)</span>
-            </div>
-          </div>
-        </li>
+      <h2 class="font-bold text-xl text-white md:text-2xl">Work Experience</h2>
+      <ul class="flex flex-col gap-6 mt-8">
+        <WorkExperienceItem
+          period="Nov. 2023 - Present"
+          company="SB Intuitions Corp."
+          companyHref="https://www.sbintuitions.co.jp/"
+          jobTitle="Engineer"
+        >
+          <li>Instruction data creation for Large Language Models (LLM)</li>
+          <li>Full stack developing for LLM applications</li>
+        </WorkExperienceItem>
+        <WorkExperienceItem
+          period="Oct. 2023 - Present"
+          company="LY Corporation"
+          companyNote="(formerly LINE Corporation)"
+          companyHref="https://www.lycorp.co.jp/"
+          jobTitle="Senior Engineer"
+        >
+          <li>Instruction data creation for Large Language Models (LLM)</li>
+          <li>Full stack developing for LLM applications</li>
+        </WorkExperienceItem>
+        <WorkExperienceItem
+          period="Apr. 2021 - Oct. 2023"
+          company="LINE Corporation"
+          companyHref="https://linecorp.com/"
+          jobTitle="Senior Engineer ← Engineer (until Sep. 2023)"
+        >
+          <li>Instruction data creation for Large Language Models (LLM)</li>
+          <li>Full stack developing for LLM applications</li>
+        </WorkExperienceItem>
+        <WorkExperienceItem
+          period="Apr. 2021 - Oct. 2023"
+          company="Mobile Factory Inc."
+          companyHref="https://www.mobilefactory.jp/"
+          jobTitle="Part-time Job"
+        >
+          <li>Instruction data creation for Large Language Models (LLM)</li>
+          <li>Full stack developing for LLM applications</li>
+        </WorkExperienceItem>
+        <WorkExperienceItem
+          period="Apr. 2021 - Oct. 2023"
+          company="Rakuten Inc."
+          companyHref="https://corp.rakuten.co.jp/"
+          jobTitle="Internship"
+        >
+          <li>Instruction data creation for Large Language Models (LLM)</li>
+          <li>Full stack developing for LLM applications</li>
+        </WorkExperienceItem>
+        <WorkExperienceItem
+          period="Apr. 2021 - Oct. 2023"
+          company="Diverta Inc."
+          companyHref="https://www.diverta.co.jp/"
+          jobTitle="Part-time Job"
+        >
+          <li>Instruction data creation for Large Language Models (LLM)</li>
+          <li>Full stack developing for LLM applications</li>
+        </WorkExperienceItem>
       </ul>
     </div>
     <div class="mt-14">
@@ -83,4 +113,6 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import WorkExperienceItem from "./components/WorkExperienceItem.vue";
+</script>
---
+++ src/components/WorkExperienceItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <li
+    class="flex flex-col md:gap-6 md:flex-row rounded-lg px-6 py-4 bg-neutral-700"
+  >
+    <div class="text-neutral-400 w-40">{{ props.period }}</div>
+    <div class="shrink flex flex-col mt-2 md:mt-0">
+      <div>
+        <span class="font-bold text-xl text-white">
+          <a
+            class="hover:text-neutral-200 hover:underline"
+            :href="props.companyHref"
+            target="_blank"
+            v-if="props.companyHref"
+            >{{ props.company }}</a
+          >
+          <span v-else>{{ props.company }}</span>
+        </span>
+        <span class="ml-2 text-neutral-400 text-sm" v-if="companyNote">{{
+          companyNote
+        }}</span>
+      </div>
+      <div class="text-neutral-400 font-bold">{{ props.jobTitle }}</div>
+      <ul class="flex flex-col list-disc list-outside ml-5 mt-2">
+        <slot />
+      </ul>
+    </div>
+  </li>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  period: string;
+  company: string;
+  companyHref?: string;
+  companyNote?: string;
+  jobTitle: string;
+}>();
+</script>
